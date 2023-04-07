5f8705dd4c92c0e2ddbb5984e505d2d2907131a3
Kubuś <jata4321@gmail.com>
2023-04-08 00:58:15 +0200

    More fun with itertools and decorators

---
+++ S009-L001 Iterables Iteratory.py
@@ -0,0 +1,40 @@
+from time import time
+import datetime as dt
+
+
+class Stoper:
+    def __init__(self, fun):
+        self.fun = fun
+
+    def __call__(self, *args, **kwargs):
+        start_time = time()
+        result = self.fun(*args, *kwargs.values())
+        [print(c) for c in result]
+        stop_time = time()
+        return print(f'Time lapsed: {stop_time - start_time}')
+
+
+@Stoper
+class DatesFromNow:
+    def __init__(self, year, month, day, count):
+        self.date = dt.date(year, month, day)
+        self.day = day
+        self.month = month
+        self.year = year
+        self.count = count
+
+    def __next__(self):
+
+        if self.count <= 0:
+            raise StopIteration()
+        else:
+            next_day = self.date
+            self.date += dt.timedelta(days=1)
+            self.count -= 1
+        return next_day
+
+    def __iter__(self):
+        return self
+
+
+calendar_day = DatesFromNow(2000, 1, 1, 365)

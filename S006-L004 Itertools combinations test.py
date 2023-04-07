6620ec5c2c074a08443debd6ad28fbf15804a6f8
Kubuś <jata4321@gmail.com>
2023-04-07 17:19:54 +0200

    More fun with itertools

--- S006-L004 Itertools combinations test.py
+++ S006-L004 Itertools combinations test.py
@@ -2,8 +2,10 @@ import itertools as it
 import math
 from itertools import combinations_with_replacement, combinations, permutations
 from math import factorial, pow
+import time
 
-notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C#', 'D#','G#','F#','A#']
+start = time.time()
+notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C#', 'D#', 'G#', 'F#', 'A#']
 notes_set = 12
 
 # for enum, combination in enumerate(permutations(notes, notes_set)):
@@ -16,16 +18,17 @@ notes_set = 12
 
 # notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
 
-for x in it.permutations(notes, 8):
+for x in it.permutations(notes, 10):
     print(x)
 
 print("4-notes melody, notes cannot repeat - it is variation without repeating - there are {} possibilities".format(
     math.factorial(len(notes)) / math.factorial(len(notes) - 4)))
-
-input('Press enter')
-
-for x in it.product(notes, repeat=4):
-    print(x)
-
-print("4-notes melody - notes can repeat - it is variation with repeating - there are {} possibilities".format(
-    pow(len(notes), 4)))
+end = time.time()
+print(f"Time elapsed: {end - start}")
+# input('Press enter')
+#
+# for x in it.product(notes, repeat=4):
+#     print(x)
+#
+# print("4-notes melody - notes can repeat - it is variation with repeating - there are {} possibilities".format(
+#     pow(len(notes), 4)))
